76a88c6aabb740772f03280155d4142b8c7e9901
clarkio <clarkio@users.noreply.github.com>
2018-12-21 13:10:20 -0500

    init to winit for clarkio version

--- bin/card.js
+++ bin/card.js
@@ -2,45 +2,65 @@
 // 👆 Used to tell Node.js that this is a CLI tool
 
 // Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
+const chalk = require("chalk");
+const boxen = require("boxen");
 
 // Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
-  borderStyle: 'round'
-}
+  borderStyle: "round"
+};
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
-  labelCard: chalk.white.bold('      Card:')
-}
+  name: chalk.white("Brian Clark /"),
+  handle: chalk.cyan("clarkio"),
+  work: chalk.white("Cloud Advocate at Microsoft"),
+  twitter: chalk.cyan("https://twitter.com/_clarkio"),
+  instagram: chalk.cyan("https://instagram.com/_clarkio"),
+  github: chalk.cyan("https://github.com/clarkio"),
+  twitch: chalk.cyan("https://twitch.tv/clarkio"),
+  web: chalk.cyan("https://clarkio.com"),
+  npx: chalk.white("npx clarkio"),
+  labelWork: chalk.white.bold("      Work:"),
+  labelTwitter: chalk.white.bold("   Twitter:"),
+  labelInstagram: chalk.white.bold(" Instagram:"),
+  labelGitHub: chalk.white.bold("    GitHub:"),
+  labelTwitch: chalk.white.bold("    Twitch:"),
+  labelWeb: chalk.white.bold("       Web:"),
+  labelCard: chalk.white.bold("      Card:")
+};
 
 // Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const newline = "\n";
+const heading = `${data.name} ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const instagraming = `${data.labelInstagram}  ${data.instagram}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const twitching = `${data.labelTwitch}  ${data.twitch}`;
+const webbing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output =
+  heading +
+  newline +
+  newline +
+  working +
+  newline +
+  twittering +
+  newline +
+  instagraming +
+  newline +
+  githubing +
+  newline +
+  twitching +
+  newline +
+  webbing +
+  newline +
+  newline +
+  carding;
 
-console.log(chalk.green(boxen(output, options)))
+console.log(chalk.green(boxen(output, options)));
